7cb87f2462d6581c674b26b11c57cbddbee38bed
David Nguyen <phuc.dav@gmail.com>
2020-05-06 10:53:48 +0200

    Set up
    fixed business

--- BusinessLogic/BusinessLogic/Controllers/WeatherForecastController.cs
+++ BusinessLogic/BusinessLogic/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace BussinessLogic.Controllers
+namespace BusinessLogic.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- BusinessLogic/BusinessLogic/WeatherForecast.cs
+++ BusinessLogic/BusinessLogic/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace BussinessLogic
+namespace BusinessLogic
 {
     public class WeatherForecast
     {
