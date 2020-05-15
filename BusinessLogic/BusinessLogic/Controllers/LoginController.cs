e5ff436d884bf1d176bf0ee15ae8aa72da287861
anderssonderby <anders.01@live.dk>
2020-05-15 15:38:40 +0200

    Update LoginController.cs

--- BusinessLogic/BusinessLogic/Controllers/LoginController.cs
+++ BusinessLogic/BusinessLogic/Controllers/LoginController.cs
@@ -32,10 +32,11 @@ namespace BusinessLogic.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
+        public async Task<ActionResult<User>> ValidateUser(User user)
         {
             
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+            BusinessSocketHandler socket = BusinessSocketHandler.getInstance();
+            socket.sendToDatabase();
         }
 
     }
