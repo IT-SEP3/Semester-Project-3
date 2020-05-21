4ad2354f921d55a80d2ab89711d84bd733d5723e
anderssonderby <anders.01@live.dk>
2020-05-21 12:26:59 +0200

    Update Shift.cs

--- BusinessLogic/BusinessLogic/Model/Calendar/Shift.cs
+++ BusinessLogic/BusinessLogic/Model/Calendar/Shift.cs
@@ -10,12 +10,10 @@ namespace BusinessLogic.Model.Calendar
     {
 
         public int Id { get; set; }
-        public DateTime CreatedTime { get; set; }
+        public int UserId { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
-        public int AssigneeId { get; set; }
-        public User Assignee { get; set; }
         public int ManagerId { get; set; }
-        public User Manager { get; set; }
+        private DateTime Date { get; set; }
     }
 }
