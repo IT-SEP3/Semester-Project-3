897ef51c118264c51a33392b71f6aa5c7f64671b
Rokasbarasa1 <50622827+Rokasbarasa1@users.noreply.github.com>
2020-05-22 09:27:40 +0200

    code cleanup

--- Client/src/main/java/model/calendar/CalendarModel.java
+++ Client/src/main/java/model/calendar/CalendarModel.java
@@ -17,28 +17,6 @@ public class CalendarModel implements ICalendarModel {
         this.calendarClient = calendarClient;
         this.loginModel = loginModel;
         shifts = new ArrayList<Shift>();
-        /*
-        Shift kaka = new Shift("19-05-2020", "Shift2");
-        Shift kaka1 = new Shift("08-05-2020", "Shift3");
-        Shift kaka2 = new Shift("03-05-2020", "Shift4");
-        Shift kaka3 = new Shift("04-05-2020", "Shift5");
-        Shift kaka4 = new Shift("09-05-2020", "Shift6");
-        Shift kaka5 = new Shift("23-05-2020", "Shift7");
-        Shift kaka6 = new Shift("28-05-2020", "Shift8");
-        Shift kaka7 = new Shift("20-05-2020", "Shift9");
-        Shift kaka8 = new Shift("12-05-2020", "Shift10");
-        Shift kaka9 = new Shift("14-05-2020", "Shift11");
-        shifts.add(kaka);
-        shifts.add(kaka1);
-        shifts.add(kaka2);
-        shifts.add(kaka3);
-        shifts.add(kaka4);
-        shifts.add(kaka5);
-        shifts.add(kaka6);
-        shifts.add(kaka7);
-        shifts.add(kaka8);
-        shifts.add(kaka9);
-         */
     }
 
     @Override
